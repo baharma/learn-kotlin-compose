30edffe8fafd95bbc3c0f17be7b29601eb84c800
baharma <bagusaditya1899@gmail.com>
2024-10-14 16:39:58 +0800

    feat(ui):basic layout

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,6 +39,34 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+@Composable
+fun userProfile() {
+    Column (modifier = Modifier){
+
+        Box(modifier = Modifier) {
+            Image(
+                painter = painterResource(R.drawable.ic_launcher_foreground),
+                contentDescription = null,
+                modifier = Modifier.size(100.dp)
+            )
+        }
+        Icon(imageVector = Icons.Default.Check, contentDescription = null)
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+private fun userProfilePreview() {
+    MyApplicationTheme{
+        userProfile()
+    }
+    
+}
+
+
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
